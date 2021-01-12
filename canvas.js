3281e17c50cc5209e2d3e09dd14b953813936dea
cbravos5 <cnbravos5@gmail.com>
2021-01-12 15:07:02 -0300

    Drag and drop functions simplified; Now supports multiple draggabble objects

--- canvas.js
+++ canvas.js
@@ -1,64 +1,98 @@
-const canvas = () => {
-    const line = document.querySelector('.draggable-line');
-    let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    const container = document.querySelector('.draw-container');
-    line.onmousedown = dragMouseDown;
 
-    const verifyTopBottomReached = top => {
-        if(top >= 0 && top <= container.clientHeight) {
-            line.style.top = top + "px";
-        } else {
-            if(top < 0){
-                line.style.top = "0px";
-            } else {
-                line.style.top = (container.clientHeight - 10) + "px";                
-            }
-        }
-    } 
+var theElement;
+var diffX, diffY;
 
-    const verifyLeftRightReached = left => {
-        if(left >= 0 && left <= container.clientWidth) {
-            line.style.left = left + "px";
+const verifyTopBottomReached = top => {
+    if(top >= 0 && top <= container.clientHeight) {
+        line.style.top = top + "px";
+    } else {
+        if(top < 0){
+            line.style.top = "0px";
         } else {
-            if(left < 0){
-                line.style.left = "0px";
-            } else {
-                line.style.left = (container.clientWidth - line.clientWidth) + "px";                
-            }
+            line.style.top = (container.clientHeight - 10) + "px";                
         }
     }
+} 
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+const verifyLeftRightReached = left => {
+    if(left >= 0 && left <= container.clientWidth) {
+        line.style.left = left + "px";
+    } else {
+        if(left < 0){
+            line.style.left = "0px";
+        } else {
+            line.style.left = (container.clientWidth - line.clientWidth) + "px";                
+        }
     }
-
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        verifyTopBottomReached(line.offsetTop - pos2);
-        verifyLeftRightReached(line.offsetLeft - pos1);
-
-    }
-
-    function closeDragElement() {
-        // stop moving when mouse button is released:
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }    
 }
-window.addEventListener("DOMContentLoaded", function () {
-    canvas();
-})
+
+function grabber(event) {
+
+    // Set the global variable for the element to be moved
+    theElement = event.currentTarget;
+
+
+    // Determine the position of the word to be grabbed,
+    //  first removing the units from left and top
+
+    var posX = parseInt(theElement.offsetLeft);
+    var posY = parseInt(theElement.offsetTop);
+    console.log(posX);
+    // Compute the difference between where it is and 
+    //  where the mouse click occurred
+
+    diffX = event.clientX - posX;
+    diffY = event.clientY - posY;
+
+    console.log(diffX);
+
+    // Now register the event handlers for moving and 
+    //  dropping the word
+    
+    document.addEventListener("mousemove", mover, true);
+    document.addEventListener("mouseup", dropper, true);
+
+    // Stop propagation of the event and stop any default 
+    //  browser action
+
+    event.stopPropagation();
+    event.preventDefault();
+
+}  //** end of grabber
+
+// *******************************************************
+
+// The event handler function for moving the word
+
+function mover(event) {
+    // Compute the new position, add the units, and move the word
+    
+    theElement.style.left = (event.clientX - diffX) + "px";
+    theElement.style.top = (event.clientY - diffY) + "px";
+    console.log(theElement.offsetLeft);
+    // Prevent propagation of the event
+
+    event.stopPropagation();
+}  //** end of mover
+
+// *********************************************************
+// The event handler function for dropping the word
+
+function dropper(event) {
+
+    // Unregister the event handlers for mouseup and mousemove
+    document.removeEventListener("mouseup", dropper, true);
+    document.removeEventListener("mousemove", mover, true);
+
+    // Prevent propagation of the event
+
+    //  event.stopPropagation();
+}   //** end of dropper
+
+window.addEventListener("DOMContentLoaded", () => {
+    const lines = document.querySelectorAll('.draggable-line');
+    lines.forEach(line => {
+        line.onmousedown = grabber;        
+    });
+    // The event handler function for grabbing the word
+});
